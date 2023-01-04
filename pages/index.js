0c7c8195f4a09233e86d0e316b5e448f7e3b3ab7
marcio.froes <marcio.froes@lello.com.br>
2023-01-03 21:46:41 -0300

    motivos queda pelo

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,17 @@ function Home(){
     <div>
         <h2>Home</h2>
             <Contador/>
+            <div>
+            <h1>5 motivos pelos quais os cães têm queda de pelos</h1>
+            <ul>
+  <li>Mudanças sazonais</li>
+  <li>Estresse</li>
+  <li>Problemas de pele</li>
+  <li>Doenças</li>
+  <li>Idade</li>
+</ul>
+</div>
+
             <div id="google-ads">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6586065875216810"
      crossorigin="anonymous"></script>
